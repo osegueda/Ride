1a257fdfea5a794f558ebdd6aad6d3ed36a5f4d5
Gerson Beltran <gmisaelbeltran@gmail.com>
2019-11-14 08:53:43 -0600

    fixing login layout

--- app/src/main/java/sv/edu/bitlab/ride/LoginActivity.kt
+++ app/src/main/java/sv/edu/bitlab/ride/LoginActivity.kt
@@ -2,12 +2,17 @@ package sv.edu.bitlab.ride
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import sv.edu.bitlab.ride.R
 
+private var img_pastillas:ImageView? = null
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        img_pastillas = findViewById(R.id.imageView2)
+        img_pastillas!!.setImageResource(R.drawable.ic_pastillas)
+
     }
 }
